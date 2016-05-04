87c4dba6baad522b90b4a04667c10d6af76aeb25
JimmyGuarin <jimmyguarin43@gmail.com>
2016-05-04 09:35:55 -0500

    1.1.1
    Esquema del motor para prueba

---
+++ Assets/Scripts/MotorPreguntas/PuntosRetro.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuntosRetro {
+
+
+    public ArrayList misPreguntas;
+    public string retroalimentacion;
+
+    public PuntosRetro(string retro)
+    {
+        misPreguntas = new ArrayList();
+        retroalimentacion = retro;
+    }
+
+
+    public void Barajar()
+    {
+        for (int i = 0; i < misPreguntas.Count; i++)
+        {
+            Preguntas temp = (Preguntas)misPreguntas[i];
+            int randomIndex = Random.Range(0, misPreguntas.Count);
+            misPreguntas[i] = misPreguntas[randomIndex];
+            misPreguntas[randomIndex] = temp;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Nivel Prueba/BullyingFisico.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BullyingFisico : MonoBehaviour {
+
+    public Nivel miNivel;
+
+
+
+    void Awake()
+    {
+        miNivel = new Nivel();
+
+        PuntosRetro p1 = new PuntosRetro("El jefe es alguien que tiene la A en su nombre, yo no programo en Ruby");
+            p1.misPreguntas.Add(new Preguntas("Quien es el Jefe", "Yo", "Felipe", "Faber", "Viviana"));
+            p1.misPreguntas.Add(new Preguntas("¿Programo en Unity?", "Si", "No"));
+            p1.Barajar();
+
+       // miNivel.puntosRetro.Add(p1);
+        toString();
+
+    }
+
+
+    public void toString()
+    {
+
+        for(int i = 0; i < miNivel.puntosRetro.Count; i++){
+
+            PuntosRetro pr = (PuntosRetro)miNivel.puntosRetro[i];
+
+            Debug.Log(pr.retroalimentacion);
+            for(int j = 0; j < pr.misPreguntas.Count;j++){
+
+                Preguntas p =(Preguntas) pr.misPreguntas[j];
+
+                Debug.Log(p.Enunciado);
+                Debug.Log("");
+                for(int k = 0; k < p.Respuestas.Length; k++){
+
+                    Debug.Log("-" + p.Respuestas[k]);
+                }
+
+            }
+
+        }
+
+    }
+
+
+}
