0b5fbdb0b23bb4b5dc66b975defc259dff50c13d
felipe2323 <felipe23_23@hotmail.com>
2016-05-05 17:44:59 -0500

    Interacciones del Escenario

---
+++ Assets/Scripts/Generadores/ActivadorCronch.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ActivadorCronch : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<UnityStandardAssets._2D.PlatformerCharacter2D>().m_Anim.SetBool("enColision", true);
+        Debug.Log("EstoyColisionando");
+
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<UnityStandardAssets._2D.PlatformerCharacter2D>().m_Anim.SetBool("Crouch", false);
+    }
+}
