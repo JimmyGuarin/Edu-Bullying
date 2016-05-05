5b641af5fdd4170c4111750e68699273f1503e96
felipe2323 <felipe23_23@hotmail.com>
2016-05-05 07:33:22 -0500

    Generacion de Obstaculos
    Se agrega la generacion de obstaculos aleatoria y adicionalmente se
    agregan unos sonidos estandar.

---
+++ Assets/Scripts/Generadores/Generador.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generador : MonoBehaviour {
+
+
+    public GameObject[] obj;
+    public float tiempoMinimo = 1f;
+    public float tiempoMaximo = 3f;
+
+
+	// Use this for initialization
+	void Start () {
+        GenerarBloques();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void GenerarBloques(){
+
+        Instantiate(obj[Random.Range(0, obj.Length)], transform.position, Quaternion.identity);
+        Invoke("GenerarBloques", Random.Range(tiempoMinimo, tiempoMaximo));
+    }
+}
