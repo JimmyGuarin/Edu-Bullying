c399ac1bb6ee7e458db1eed87876aababb0dfa1f
JimmyGuarin <jimmyguarin43@gmail.com>
2016-05-11 16:18:40 -0500

    1.5
    Fin de Motores

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+//using System.Collections;
+
+public class Enemigo : MonoBehaviour
+{
+
+    public GameObject presionarFPanel;
+    public GameObject canvasConversacion;
+    public MaquinaEstadosConver mq;
+    public bool enColision;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        enColision = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (enColision)
+        {
+
+            if (Input.GetKeyDown("f"))
+            {
+                Time.timeScale = 0;
+                presionarFPanel.SetActive(false);
+                canvasConversacion.SetActive(true);
+                mq.Moldear();
+                GetComponent<Collider2D>().enabled = false;
+                enColision = false;
+            }
+        }
+    }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+
+        presionarFPanel.transform.GetChild(0).GetChild(0).GetComponent<Text>().text ="Para hablar con "+gameObject.name+" y convencerlo"+
+            "de dejar el Bullying presiona F";
+        presionarFPanel.SetActive(true);
+        enColision = true;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        presionarFPanel.SetActive(false);
+        enColision = false;
+    }
+}
---
+++ Assets/Scripts/MotorConversaciones/MaquinaEstadosConver.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MaquinaEstadosConver : MonoBehaviour{
+
+
+    public EstadoConversacion[] estados;
+    private int estadoActual;
+
+    public Sprite imagenProfesora;
+    public Sprite imagenVillano;
+
+
+    public GameObject canvasPlayerNpc;
+    public Image personajePlayerNpc;
+    public Text textoPlayerNpc;
+    public Button[] botonesNpc;
+
+    void Start()
+    {
+        estadoActual = 0;
+    }
+
+
+    public void CambiarEstado(int i)
+    {
+        estadoActual = estados[estadoActual].proximos[i];
+        Moldear();
+    }
+
+    public void Moldear()
+    {
+        EstadoConversacion converActual= estados[estadoActual];
+        
+
+        switch (converActual.tipo)
+        {
+            case 0:
+                personajePlayerNpc.sprite = imagenProfesora;
+                textoPlayerNpc.text = converActual.pregunta.Enunciado;
+                for(int i = 0; i <3; i++)
+                {
+
+                    if (converActual.proximos!=null&& converActual.proximos.Length > 3)
+                    {
+                       
+                        botonesNpc[i].onClick.AddListener(() => { CambiarEstado(converActual.proximos[i]); });
+                    }
+                    botonesNpc[i].GetComponentInChildren<Text>().text = converActual.pregunta.Respuestas[i];
+
+
+                }
+                canvasPlayerNpc.SetActive(true);
+                break;
+
+        }
+    }
+
+
+
+}
