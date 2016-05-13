0bc4842cf8e4262ab4c45c2b5df12636c1a8cea7
felipe2323 <felipe23_23@hotmail.com>
2016-05-13 11:51:27 -0500

    Patio v1.0
    Se termina la primera escena del patio, con algunos detalles que falta
    por agregar.

---
+++ Assets/Scripts/Patio/AnclandoPersonaje.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+public class AnclandoPersonaje : MonoBehaviour
+{
+
+
+
+    //Hace hijo al personaje de la plataforma en el momento en que entra en colision con ella
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "PlataformasMov")
+        {
+            gameObject.transform.SetParent(collision.gameObject.transform);
+
+        }
+
+        if (collision.gameObject.tag == "Trampolin")
+        {
+            //GetComponent<PlatformerCharacter2D>().m_JumpForce = 0f;
+            Debug.Log(GetComponent<PlatformerCharacter2D>().m_JumpForce);
+        }
+
+
+    }
+    //Elimina el padre del personje.
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "PlataformasMov")
+        {
+            gameObject.transform.parent = null;
+        }
+        
+
+    }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        
+    }
+}
