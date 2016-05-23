1b72a945572f815ed46c1744c156b2d1e1b2c0ec
felipe2323 <felipe23_23@hotmail.com>
2016-05-23 09:39:48 -0500

    Restaurante V3.0
    Optimizacion de Colliders, fondos , vacios y pisos statics.

--- Assets/Scripts/Generadores/Generador.cs
+++ Assets/Scripts/Generadores/Generador.cs
@@ -7,6 +7,7 @@ public class Generador : MonoBehaviour {
     public GameObject[] obj;
     public float tiempoMinimo = 1f;
     public float tiempoMaximo = 2f;
+    public float maxDistance = 25f;
 
     private Vector3 posicionInic;
 
@@ -30,12 +31,12 @@ public class Generador : MonoBehaviour {
 
 
         //transform.position = Vector3.MoveTowards(transform.position, new Vector3(posicionInic.x + Random.Range(0, 15), posicionInic.y, 0), 0.2f);
-        transform.position = new Vector3(posicionInic.x + Random.Range(0,25), posicionInic.y, 0);
+        transform.position = new Vector3(posicionInic.x + Random.Range(0,maxDistance), posicionInic.y, 0);
     }
 
     void GenerarBloques(){
 
-        Instantiate(obj[Random.Range(0, obj.Length)], transform.position, Quaternion.identity);
+        Instantiate(obj[Random.Range(0, obj.Length)], transform.position, Quaternion.AngleAxis(Random.Range(0,360), Vector3.back));
         Invoke("GenerarBloques", Random.Range(tiempoMinimo, tiempoMaximo));
     }
 }
