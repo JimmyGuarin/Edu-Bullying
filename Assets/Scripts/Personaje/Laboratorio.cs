d0eb0a48619a6c4ac47e9ee8cc9bfa71c7e9225f
JimmyGuarin <jimmyguarin43@gmail.com>
2016-05-26 15:57:29 -0500

    Laboratorio
    Construcción del Laboratorio

---
+++ Assets/Scripts/Personaje/Laboratorio.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laboratorio : MonoBehaviour
+{
+
+    private MovimientoPersonaje mp;
+    public float escalaMax;
+    public float escalaMin;
+    public float tasaCrecimiento;
+
+    private bool crecer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        mp = GetComponent<MovimientoPersonaje>();
+        crecer = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (crecer)
+        {
+
+
+            //mp.escala -= (tasaCrecimiento / 2) * Time.deltaTime;
+            if (mp.escala < escalaMax)
+            {
+
+            }
+            mp.escala += tasaCrecimiento;
+            Debug.Log(mp.escala);
+        }
+
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.name.Equals("ColisionGrande") && Mathf.Abs(mp.escala) <= escalaMax)
+        {
+
+            if (transform.localScale.x < 0)
+                transform.localScale = new Vector3(transform.localScale.x - (tasaCrecimiento * 2 * Time.deltaTime), transform.localScale.y + (tasaCrecimiento * 2 * Time.deltaTime), 1);
+            else
+            {
+                transform.localScale = new Vector3(transform.localScale.x + (tasaCrecimiento * 2 * Time.deltaTime), transform.localScale.y + (tasaCrecimiento * 2 * Time.deltaTime), 1);
+            }
+            mp.escala += tasaCrecimiento * 2 * Time.deltaTime;
+            mp.velocidad = 10;
+            mp.fuerzaSalto = 30;
+            mp.radioValidacion = (float)((0.45 * mp.escala) / 0.2);
+        }
+
+
+        if (collision.tag.Equals("Destructor"))
+        {
+            Debug.Log("Entra");
+            mp.escala = 0.2f;
+            mp.velocidad = 8;
+            mp.fuerzaSalto = 25;
+            transform.localScale = new Vector3(0.2f, 0.2f, 1);
+            mp.radioValidacion = 0.45f;
+        }
+
+
+        if (collision.name.Equals("ColisionPequeña") && Mathf.Abs(mp.escala) >= escalaMin)
+        {
+
+            if (transform.localScale.x < 0)
+                transform.localScale = new Vector3(transform.localScale.x + (tasaCrecimiento * 4 * Time.deltaTime), transform.localScale.y - (tasaCrecimiento * 4 * Time.deltaTime), 1);
+            else
+                transform.localScale = new Vector3(transform.localScale.x - (tasaCrecimiento * 4 * Time.deltaTime), transform.localScale.y - (tasaCrecimiento * 4 * Time.deltaTime), 1);
+            mp.escala -= tasaCrecimiento * 4 * Time.deltaTime;
+            mp.velocidad = 8;
+            mp.fuerzaSalto = 25;
+            mp.radioValidacion = 0.45f;
+
+        }
+
+
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name.Equals("ColisionPequeña"))
+        {
+
+            mp.escala = 0.2f;
+        }
+    }
+}
--- Assets/Scripts/Personaje/MovimientoPersonaje.cs
+++ Assets/Scripts/Personaje/MovimientoPersonaje.cs
@@ -1,45 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MovimientoPersonaje : MonoBehaviour {
+public class MovimientoPersonaje : MonoBehaviour
+{
 
-	public float fuerzaSalto=15f;
-	public float velocidad = 5f;
-	private Rigidbody2D rb;
-	private bool tocaPiso;
-	private Animator anim;
-	public LayerMask capaPiso;
-	public float radioValidacion;
-	public Transform validadorPiso;
-    public float escala=0.2f;
+    public float fuerzaSalto = 15f;
+    public float velocidad = 5f;
+    private Rigidbody2D rb;
+    private bool tocaPiso;
+    private Animator anim;
+    public LayerMask capaPiso;
+    public float radioValidacion;
+    public Transform validadorPiso;
+    public float escala = 0.2f;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
         tocaPiso = true;
-		rb = GetComponent<Rigidbody2D> ();
-		anim = GetComponent<Animator> ();
-		anim.SetInteger ("Estado", 0);
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        anim.SetInteger("Estado", 0);
     }
 
     void FixedUpdate()
     {
-        
-        tocaPiso = Physics2D.OverlapCircle(validadorPiso.position, radioValidacion, capaPiso);
 
-
+      
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
+        //tocaPiso = Physics2D.OverlapCircle(validadorPiso.position, radioValidacion, capaPiso);
+        tocaPiso = Physics2D.OverlapArea(new Vector2(validadorPiso.position.x - radioValidacion, validadorPiso.position.y + (radioValidacion / 8)), new Vector2(validadorPiso.position.x + radioValidacion, validadorPiso.position.y - (radioValidacion / 8)), capaPiso, 0);
+        
 
         if (tocaPiso)
         {
             anim.SetInteger("Estado", 0);
+
         }
 
-       
-    
+
+
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -59,53 +64,60 @@ public class MovimientoPersonaje : MonoBehaviour {
             if (tocaPiso)
                 anim.SetInteger("Estado", 1);
         }
-        
-        if (Input.GetKey(KeyCode.UpArrow)&&(tocaPiso))
+
+        if (Input.GetKey(KeyCode.UpArrow) && (tocaPiso))
         {
-                
-              
-                    anim.enabled = false;
-                    anim.enabled = true;
-                    anim.SetInteger("Estado", 2);
-                    if(tocaPiso)
-                        rb.velocity = new Vector2(rb.velocity.x,fuerzaSalto);
-                    tocaPiso = false;
-            
-               
+
+
+            anim.enabled = false;
+            anim.enabled = true;
+            anim.SetInteger("Estado", 2);
+            // if (tocaPiso)
+            rb.velocity = new Vector2(rb.velocity.x, fuerzaSalto);
         }
 
-  
-    }
-
-    
-
-    void OnCollisionEnter2D(Collision2D collider){
-
-
-        
-
-		if (collider.transform.tag.Equals ("PlataformasMov")) {
-		
-			transform.parent = collider.transform;
-       
-		}
-
-       
 
 
     }
 
-   
 
 
-        void OnCollisionExit2D(Collision2D collider){
+    void OnCollisionEnter2D(Collision2D collider)
+    {
 
-		if (collider.transform.tag.Equals ("PlataformasMov")) {
 
-			transform.parent = null;
-		}
 
-       
+
+        if (collider.transform.tag.Equals("PlataformasMov"))
+        {
+
+            transform.parent = collider.transform;
+
+        }
+
+
+
 
     }
+
+
+
+
+    void OnCollisionExit2D(Collision2D collider)
+    {
+
+        if (collider.transform.tag.Equals("PlataformasMov"))
+        {
+
+            transform.parent = null;
+        }
+
+
+
+    }
+
+    //void OnDrawGizmos()
+    //{
+    //    Gizmos.DrawWireCube(validadorPiso.position, new Vector3(radioValidacion, radioValidacion / 8, 1));
+    //}
 }
