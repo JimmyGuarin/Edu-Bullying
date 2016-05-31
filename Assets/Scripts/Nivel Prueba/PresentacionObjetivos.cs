8a6edb5e6049120a616a59f7bd6fa0a500fe867a
JimmyGuarin <jimmyguarin43@gmail.com>
2016-05-31 15:39:09 -0500

    Retoques Niveles
    -Corredor
    -SalonClases
    -Patio1
    -Restaurante

---
+++ Assets/Scripts/Nivel Prueba/PresentacionObjetivos.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PresentacionObjetivos : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public void OnDisable()
+    {
+        Time.timeScale = 1;
+    }
+}
