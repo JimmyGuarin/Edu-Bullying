691f7b62beb83f5c2742b1ed0077f0f7227e2e91
JimmyGuarin <jimmyguarin43@gmail.com>
2016-06-01 09:01:03 -0500

    Mejoras al Seleccionar Personaje
    Se mejora la interacción a la hora de seleccionar un personaje.

---
+++ Assets/Scripts/MenusGUI/OffPersonajes.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffPersonajes : MonoBehaviour
+{
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    public void Activar()
+    {
+        if (this.gameObject.activeSelf == true)
+        {
+
+            if (anim.GetInteger("Estado") != 1)
+            {
+                anim.SetInteger("Estado", 1);
+            }
+        }
+    }
+
+    public void Desactivar()
+    {
+        if (this.gameObject.activeSelf == true)
+        {
+
+            if (anim.GetInteger("Estado") != 2)
+            {
+                anim.SetInteger("Estado", 2);
+            }
+        }
+
+    }
+
+
+    public void OnDisable()
+    {
+        GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+    }
+}
