3c826f83696ed04cf403d57fb2d6de55d0efbe70
JimmyGuarin <jimmyguarin43@gmail.com>
2016-06-07 15:09:30 -0500

    Tabla Puntajes
    Se implementa el sistema de puntajes con una tabla de posiciones de 1 a
    5

---
+++ Assets/Scripts/MenusGUI/ControladorPuntajes.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ControladorPuntajes : MonoBehaviour {
+
+
+    
+
+
+	// Use this for initialization
+	void Start () {
+	
+
+        for(int i = 1; i < 6; i++)
+        {
+            transform.GetChild(i-1).GetChild(0).GetComponent<Text>().text= ""+ PlayerPrefs.GetString("nombre"+i)+" "+PlayerPrefs.GetInt("highscorePos" + i);
+        }
+        if (ControladorHUD.posicionPuntajes != 0)
+        {
+            transform.GetChild(ControladorHUD.posicionPuntajes - 1).GetChild(0).GetComponent<Text>().color = Color.red;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
